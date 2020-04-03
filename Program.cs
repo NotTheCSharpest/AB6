a1e96e3ba4d5be0a8da5c45fbfb6deb78e4ff51b
Soshi <soshi.mimicry@gmail.com>
2020-04-03 15:41:26 +0200

    trivial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AB6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Indien = 700000000;
+            double China = 990000000;
+            int i = 1987;
+            while (Indien < China)
+            {
+                Indien = Indien * .021;
+                China = China * .014;
+                i++;
+            }
+            Console.WriteLine("Bevolkerungsanzahl Indien: {0} \n Bevolkerungsanzahl China: {1} \n Indien ist grosser in Jahr {2}", Indien, China, i);
+                    }
+    }
+}
